54a833252c26624f0b2fb2c8ffcbf2ccfaed224d
ayiio <61615400+ayiio@users.noreply.github.com>
2023-08-30 23:48:51 +0800

    Update mydb.sql

--- mysql/mydb.sql
+++ mysql/mydb.sql
@@ -4,3 +4,7 @@ CREATE TABLE `user` (
   `age` INT(11) DEFAULT '0',
   PRIMARY KEY(`id`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+
+insert into user (name, age) values('test1', 10);
+insert into user (name, age) values('test2', 20);
